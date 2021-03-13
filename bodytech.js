0656aca0804717269b5e9cef855fe14e1ba96982
dayanabm2817 <77558981+dayanabm2817@users.noreply.github.com>
2021-03-12 20:59:46 -0500

    PROYECTO JS

---
+++ bodytech.js
@@ -0,0 +1,22 @@
+
+
+//1.MONTOS DE PAGOS 
+let pagox40Horas="20000";
+let pagoConHorasExtra="25000";
+
+//2.HORAS TRABAJADAS POR SEMANA 
+let semana1=50;
+let semana2=40;
+
+//3.DATOS 
+let empresa="postobon";
+let nombreDelTrabajador="Juan Perez";
+let IdTrabajador="123456789";
+
+//PROCESO
+semana1=1250000;
+semana2=80000;
+
+//SALIDAS DEL PROBLEMA
+console.log(semana1+"Es la semana 1");
+console.log("La semana1 es $(semana1)} yo soy un memsaje")
